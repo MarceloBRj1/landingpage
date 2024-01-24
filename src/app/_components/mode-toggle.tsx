ac51c5569ac09668e598c01a14f9d56196fdf9a7
MarceloBRj1 <marcelobarreira19@hotmail.com>
2024-01-24 17:32:02 -0300

    first commit

---
+++ src/app/_components/mode-toggle.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { useTheme } from "next-themes"
